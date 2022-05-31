be6ed54d83360fe6188c24f96551ae236835b18a
AaronTran206 <aarontran.wa@gmail.com>
2022-05-31 03:56:08 -0600

    perks responsiveness fixes

--- src/perks/Perks.tsx
+++ src/perks/Perks.tsx
@@ -8,11 +8,11 @@ const Perks: React.FC<{}> = ({}) => {
       <div className="perks__header ">
         <h1>Cursed Perks</h1>
       </div>
-      <div className="row justify-content-center g-3">
+      <div className="row justify-content-center">
         {cursedPerks.map((perk) => (
           <div
             key={perk.name}
-            className="col-xs-12 col-sm-5 col-md-3 perks__card"
+            className="col-xs-10 col-sm-4 col-md-3 perks__card"
           >
             <h2 className={"perks__name"}>{perk.name}</h2>
             <h5 className={"perks__rules"}>{perk.rules}</h5>
